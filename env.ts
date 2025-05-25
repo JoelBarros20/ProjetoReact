c4e6272890bd1fed11360cb5254e135218803498
JoelBarros20 <pv22460@alunos.estgv.ipv.pt>
2025-05-25 19:34:20 +0100

    update

--- env.ts
+++ env.ts
@@ -1,7 +1,7 @@
 // Ficheiro env para colocar variáveis sensiveis 
 
 // IP e Porta 
-const LOCAL_IP = "192.168.1.86";
+const LOCAL_IP = "192.168.1.117";
 const PORT = "8000";
 
 // Construção do URL com o IP e a Porta
